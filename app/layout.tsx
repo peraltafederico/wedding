68b9a4b54455509fff77fcfbadfe19f516e31411
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-08 20:45:13 -0300

    Add sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import '@/styles/globals.css';
 import { Metadata, Viewport } from 'next';
 import clsx from 'clsx';
 
+import AOSinit from '../components/aos';
+import { Navbar } from '../components/navbar';
+
 import { Providers } from './providers';
 
 import { siteConfig } from '@/config/site';
-import { fontSans } from '@/config/fonts';
+import { fontSans, starWhite, youngSerif } from '@/config/fonts';
 
 export const metadata: Metadata = {
   title: {
@@ -29,10 +32,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html suppressHydrationWarning lang='en'>
       <head />
-      <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable,
+          starWhite.variable,
+          youngSerif.variable
+        )}>
+        <AOSinit />
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           <div className='relative flex flex-col h-screen'>
+            <Navbar />
             <main className='w-full'>{children}</main>
+            {/* <footer className='w-full flex items-center justify-center py-3'>
+              <Link
+                isExternal
+                className='flex items-center gap-1 text-current'
+                href='https://nextui-docs-v2.vercel.app?utm_source=next-app-template'
+                title='nextui.org homepage'>
+                <span className='text-default-600'>Powered by</span>
+                <p className='text-primary'>NextUI</p>
+              </Link>
+            </footer> */}
           </div>
         </Providers>
       </body>
---
+++ components/history-section.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React from 'react';
+
+import {} from '@nextui-org/react';
+import NextImage from 'next/image';
+
+import leftFlower1 from '../assets/left-flower-1.png';
+
+import Section from './section';
+
+function HistorySection() {
+  return (
+    <Section>
+      <div className='relative'>
+        <NextImage
+          alt='Default Image'
+          src={leftFlower1}
+          style={{
+            opacity: 0.25,
+            position: 'absolute',
+            left: -92,
+            top: '-3rem',
+            maxWidth: 'unset',
+            width: '300px',
+          }}
+        />
+        <div className='flex flex-col gap-6 text-center'>
+          <div className='font-bold text-2xl flex flex-col'>
+            <h2>
+              <span className='block'>{`Todo empezó con un`}</span>
+              <span className='block'>{`"Me Gusta"`}</span>
+            </h2>
+          </div>
+          <p className='leading-loose'>
+            En tiempos de Facebook nos conocimos a traves de un ¨Me Gusta¨, en esos estados de canciones que uno
+            publicaba en el muro. Nuestros caminos se cruzaron siendo adolescentes, crecimos a la par y formamos a lo
+            largo de estos 10 años una hermosa relaciasdón de compañerismo, amistad, amor y respeto. Hoy queremos
+            festejar el amor que nos tenemos el uno para el otro con las personas que formaron y forman parte importante
+            en nuestras vidas. <span className='text-mossGreen'>Y vos sos una de ellas.</span>
+          </p>
+          <p className='font-whiteStar text-3xl text-mossGreen'>Listo para formar parte de nuestra celebracion?</p>
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+export default HistorySection;
