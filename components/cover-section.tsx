c98f351bf5e7afb4fc8075aedbdca6575bb15d35
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-08 22:16:47 -0300

    Fix scroll

--- components/cover-section.tsx
+++ components/cover-section.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import React from 'react';
+import NextImage from 'next/image';
+
+import cover from '../assets/cover.jpg';
 
 function CoverSection() {
   return (
-    <section className='w-screen h-dvh'>
-      <div className={`absolute w-full h-full bg-[url('/cover.jpg')] bg-fixed bg-center bg-cover`}>
-        {/* <NextImage
+    <section className='w-screen h-screen'>
+      <div className={`absolute w-full h-full`}>
+        <NextImage
           fill
           alt='Default Image'
           src={cover}
@@ -14,7 +17,7 @@ function CoverSection() {
             backgroundPositionY: 'center',
             objectFit: 'cover',
           }}
-        /> */}
+        />
       </div>
       <div className='absolute w-full h-full flex'>
         <div className='font-whiteStar flex text-center text-4xl flex-col mt-auto text-white w-full mb-8 gap-6'>
