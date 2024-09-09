065a2b621025b73fd0e27b1d58e84e6baf329912
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-08 21:54:42 -0300

    Fix overflow

--- components/cover-section.tsx
+++ components/cover-section.tsx
@@ -16,7 +16,7 @@ function CoverSection() {
           }}
         /> */}
       </div>
-      <div className='absolute w-full h-full flex' data-aos='zoom-out'>
+      <div className='absolute w-full h-full flex'>
         <div className='font-whiteStar flex text-center text-4xl flex-col mt-auto text-white w-full mb-8 gap-6'>
           <h1 className='leading-snug'>Cami y Fede</h1>
           <h1 className='leading-snug'>Nos Casamos!</h1>
