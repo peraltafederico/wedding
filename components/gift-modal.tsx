8ac6f60bafae6dfea406e808a07e400415805ecc
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-16 12:24:54 -0300

    Translate modal

--- components/gift-modal.tsx
+++ components/gift-modal.tsx
@@ -1,5 +1,6 @@
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Card } from '@nextui-org/react';
 import { Divider } from '@nextui-org/divider';
+import { useTranslations } from 'next-intl';
 
 type Props = {
   isOpen: boolean;
@@ -7,6 +8,8 @@ type Props = {
 };
 
 function GiftModal({ isOpen, onOpenChange }: Props) {
+  const t = useTranslations('gift.modal');
+
   return (
     <Modal
       hideCloseButton
@@ -19,7 +22,7 @@ function GiftModal({ isOpen, onOpenChange }: Props) {
         {onClose => (
           <>
             <ModalHeader className='flex flex-col gap-1 md:text-[1.125em] md:py-[1em] md:px-[1.5em]'>
-              ¡Gracias por tu regalo!
+              {t('title')}
             </ModalHeader>
             <ModalBody className='md:py-[0.5em] md:px-[1.5em]'>
               <div>
@@ -28,14 +31,11 @@ function GiftModal({ isOpen, onOpenChange }: Props) {
                 <p className='md:text-[1em]'>Alias: noscasamos.camiyfede</p>
               </div>
               <Divider />
-              <p>
-                Si preferís hacernos tu presente el día de nuestra boda vas a disponer de una alcancía en la entrada de
-                la ceremonia. \uD83D\uDC95
-              </p>
+              <p>{t('note')} \uD83D\uDC95</p>
             </ModalBody>
             <ModalFooter className='md:py-[1em] md:px-[1.5em]'>
               <Button className='md:text-[1em] md:min-w-[5em] md:h-[2.5em] font-bold' color='primary' onPress={onClose}>
-                Volver
+                {t('cta')}
               </Button>
             </ModalFooter>
           </>
