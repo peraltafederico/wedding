9bf74725a57b1af97fd854e5be13eb09609671d8
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-16 17:14:17 -0300

    Fix sizes

--- components/config.tsx
+++ components/config.tsx
@@ -42,27 +42,30 @@ function Config() {
 
   return (
     <div className='fixed z-10 right-0'>
-      <div className='p-2 flex gap-3'>
+      <div className='p-2 flex gap-2'>
         <FontAwesomeIcon
-          className='w-5 md:w-3 text-light select-none transition-all duration-300'
+          className='md:w-3 px-2 md:px-0 text-light select-none transition-all duration-300'
           icon={isSound ? faVolumeMute : faVolumeHigh}
           role='button'
+          size='lg'
           onClick={handleSound}
         />
         <FontAwesomeIcon
-          className='w-3 md:w-2 text-light select-none transition-all duration-300'
+          className='md:w-2 px-2 md:px-0 text-light select-none transition-all duration-300'
           icon={isLight ? faMoon : faSun}
           role='button'
+          size='lg'
           onClick={handleTheme}
         />
         <FontAwesomeIcon
-          className={clsx('w-5 md:w-4 text-light select-none', {
+          className={clsx('md:w-4 md:px-0 px-2 text-light select-none', {
             'opacity-50': isPending,
             'cursor-not-allowed': isPending,
             'transition-all duration-300': !isPending,
           })}
           icon={faLanguage}
           role='button'
+          size='lg'
           onClick={handleLanguage}
         />
       </div>
