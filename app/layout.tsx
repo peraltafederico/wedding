a582a9b108327e55e184d09ed72cabc7fd22e069
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-15 23:53:54 -0300

    Remove code

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,18 +100,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <SpeedInsights />
         <AOSinit />
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'light' }}>
-          {/* <Navbar /> */}
           <Snap>{children}</Snap>
-          {/* <footer className='w-full flex items-center justify-center py-3'>
-              <Link
-                isExternal
-                className='flex items-center gap-1 text-current'
-                href='https://nextui-docs-v2.vercel.app?utm_source=next-app-template'
-                title='nextui.org homepage'>
-                <span className='text-default-600'>Powered by</span>
-                <p className='text-primary'>NextUI</p>
-              </Link>
-            </footer> */}
         </Providers>
       </body>
     </html>
