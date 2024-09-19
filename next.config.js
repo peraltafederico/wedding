b0111fe66e380606e8df992c3ec254662682b073
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-09-19 13:56:00 -0300

    Disable turbopack

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        // Disable turbo to make mixpanel work
+        turbo: false
+    }
+};
  
 module.exports = withNextIntl(nextConfig);
