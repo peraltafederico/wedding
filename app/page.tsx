8d08617f5b78e00745cda79e40690bf90d7add7a
Federico Peralta <peralta.federico.manuel@gmail.com>
2024-12-04 20:40:10 -0300

    Use svh

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Instagram from '../components/instagram';
 export default function Home() {
   return (
     <>
-      <div className='h-screen w-full bg-black'>
+      <div className='h-svh w-full bg-black'>
         <div className='h-full w-full relative overflow-hidden bg-red opacity-70 flex justify-center items-center'>
           <Feedback />
         </div>
